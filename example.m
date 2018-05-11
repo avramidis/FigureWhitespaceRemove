baccac9267626c1eaf72cf4991484d49596d8cef
Eleftherios Avramidis <el.avramidis@gmail.com>
2018-05-11 22:12:36 +0100

    Corrected comments.

--- example.m
+++ example.m
@@ -1,19 +1,20 @@
 close all
 clear variables
 
-% Create and example figure
+%% Create and example figure
 createTestFigure()
 
-% Process and save the figure
+%% Process and save the figure
 iminfo=saveFigure4PLOS('testFigure');
 
-% Calculate width and height in inches
+%% Calculate width and height in inches
 width=iminfo.Width/iminfo.XResolution;
 height=iminfo.Height/iminfo.YResolution;
 
-% Check size
+%% Check size
 % Maximum: 7.5 in (19.05 cm) W x 8.75 in (22.23 cm) H
 % Minimum: 2.63 in (6.68 cm) W
+
 % Check width
 if width<2.63 || width>7.5
     disp('Width of image out of bounds!')
